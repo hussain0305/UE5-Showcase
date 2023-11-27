9b2a5e0caad86f4d95ce36f86ec28fb8f3951dd5
Hussain Shauzab Abis <hussain0305@users.noreply.github.com>
2023-11-27 21:31:56 +0200

    Character Class created, Enhanced Input setup with Move, Jump and Look functionality

--- Source/OmniProject/Private/ACPawn.cpp
+++ Source/OmniProject/Private/ACPawn.cpp
@@ -1,5 +1,6 @@
 #include "ACPawn.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h" 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
@@ -7,19 +8,20 @@ AACPawn::AACPawn()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Setup Base Mesh
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(FName("Base Mesh"));
+	SetRootComponent(SkeletalMesh);
+
 	//Setup Capsule Component
 	BaseCapsule = CreateDefaultSubobject<UCapsuleComponent>(FName("Base Collision"));
 	BaseCapsule->SetCapsuleHalfHeight(44.0f);
 	BaseCapsule->SetCapsuleRadius(22.0f);
-	SetRootComponent(BaseCapsule);
-
-	//Setup Base Mesh
-	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Base Mesh"));
-	BaseMesh->SetupAttachment(GetRootComponent());
+	BaseCapsule->SetupAttachment(GetRootComponent());
 
 	//Setup Camera Boom
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(FName("Camera Boom"));
 	CameraBoom->TargetArmLength = 300.0f;
+	CameraBoom->SetupAttachment(GetRootComponent());
 
 	//Setup Camera
 	Camera = CreateDefaultSubobject<UCameraComponent>(FName("Camera"));
@@ -44,3 +46,12 @@ void AACPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
 
+void AACPawn::Move(const FInputActionValue& Value)
+{
+
+}
+
+void AACPawn::Look(const FInputActionValue& Value)
+{
+
+}
--- Source/OmniProject/Public/ACPawn.h
+++ Source/OmniProject/Public/ACPawn.h
@@ -2,11 +2,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "ACPawn.generated.h"
 
 class UCapsuleComponent;
 class USpringArmComponent;
 class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class OMNIPROJECT_API AACPawn : public APawn
@@ -16,23 +19,41 @@ class OMNIPROJECT_API AACPawn : public APawn
 public:
 	AACPawn();
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Core Component")
-		UStaticMeshComponent* BaseMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Core Component")
+		USkeletalMeshComponent* SkeletalMesh;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Core Component")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Core Component")
 		UCapsuleComponent* BaseCapsule;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Core Component")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Core Component")
 		USpringArmComponent* CameraBoom;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Core Component")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Core Component")
 		UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputMappingContext* MappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* InputAction_Move;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* InputAction_Look;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* InputAction_Jump;
+
 protected:
 	virtual void BeginPlay() override;
+
+	void Move(const FInputActionValue& Value);
+
+	void Look(const FInputActionValue& Value);
 
 public:	
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 
 };
