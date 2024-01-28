ee6430e9252ff5ebd55b057ad42823c16ad918a5
Hussain Shauzab Abis <hussain0305@users.noreply.github.com>
2024-01-28 20:51:30 +0200

    [WIP] GAS Attributes added

--- Source/OmniProject/Private/GameplayAbilitySystem/OmniAttributeSet.cpp
+++ Source/OmniProject/Private/GameplayAbilitySystem/OmniAttributeSet.cpp
@@ -2,4 +2,26 @@
 
 
 #include "GameplayAbilitySystem/OmniAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UOmniAttributeSet::UOmniAttributeSet()
+{
+}
+
+void UOmniAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UOmniAttributeSet, Health,		COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UOmniAttributeSet, MaxHealth,	COND_None, REPNOTIFY_Always);
+}
+
+void UOmniAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UOmniAttributeSet, Health, OldHealth);
+}
+
+void UOmniAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UOmniAttributeSet, MaxHealth, OldMaxHealth);
+}
--- Source/OmniProject/Public/GameplayAbilitySystem/OmniAttributeSet.h
+++ Source/OmniProject/Public/GameplayAbilitySystem/OmniAttributeSet.h
@@ -4,14 +4,41 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "OmniAttributeSet.generated.h"
 
-/**
- * 
- */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class OMNIPROJECT_API UOmniAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UOmniAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	
+//====================================
+//Attributes and their OnRep functions
+//====================================
+	//--------
+	// Health
+	//--------
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Health, Category="Character Attributes")
+		FGameplayAttributeData Health;
+		ATTRIBUTE_ACCESSORS(UOmniAttributeSet, Health);
+	UFUNCTION()
+		void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	//-----------
+	// Max Health
+	//-----------
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxHealth, Category="Character Attributes")
+		FGameplayAttributeData MaxHealth;
+		ATTRIBUTE_ACCESSORS(UOmniAttributeSet, MaxHealth);
+	UFUNCTION()
+		void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
 };
